94075ec49b9405c74c8b609df7fbab9c6e9a5a07
Abhishek Bharadwaj <abhishek.bharadwaj@healthify.co>
2019-03-10 15:03:24 +0530

    Added category ui model

--- app/src/main/java/com/abhishek/chitrashala/data/preferences/AppPreference.kt
+++ app/src/main/java/com/abhishek/chitrashala/data/preferences/AppPreference.kt
@@ -3,22 +3,12 @@ package com.abhishek.chitrashala.data.preferences
 import android.content.Context
 import com.abhishek.chitrashala.ChitraShalaApp
 import com.abhishek.chitrashala.base.BaseSharedPreference
-import java.util.*
 
 class AppPreference : BaseSharedPreference(ChitraShalaApp.context.getSharedPreferences(
     "app_pref", Context.MODE_PRIVATE)) {
 
     companion object {
-        private const val KEY_SUBSCRIBED_SET = "subscribed_set"
         private const val KEY_POST_AFTER = "post_after"
-    }
-
-    fun saveSubscribedSubreddits(subscribed: LinkedHashSet<String>) {
-        getEditior().putStringSet(KEY_SUBSCRIBED_SET, subscribed).apply()
-    }
-
-    fun getSubscribedSubreddits() {
-        getPref().getStringSet(KEY_SUBSCRIBED_SET, emptySet())
     }
 
     fun savePostAfter(postAfter: String) = getEditior().putString(KEY_POST_AFTER, postAfter).apply()
---
+++ app/src/main/java/com/abhishek/chitrashala/ui/adapter/CategoryUIModel.kt
@@ -0,0 +1,34 @@
+package com.abhishek.chitrashala.ui.adapter
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class CategoryUIModel(val id: String,
+                      val displayName: String,
+                      val activityUserCount: Int,
+                      val allowImages: Boolean,
+                      val bannerBgImage: String,
+                      val bannerBgColor: String,
+                      val description: String,
+                      val primaryColor: String,
+                      val createdAt: String,
+                      val headerTitle: String,
+                      val isOver18: Boolean,
+                      val subscribers: Int,
+                      val publicDescription: String,
+                      val url: String,
+                      val iconImage: String) : Parcelable {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CategoryUIModel) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
